09ba01821f8207f251a61bccb874100e176aebd0
morpheous <morpheous@morpheouss-MacBook-Air.local>
2019-10-12 21:38:46 -0700

    4

--- test/websocket_test.py
+++ test/websocket_test.py
@@ -1,8 +1,9 @@
 import websocket
 import json
 ws = websocket.WebSocket()
-ws.connect( "ws://127.0.0.1:6161" )
-#ws.send( "tdReady" )
+#ws.connect( "ws://127.0.0.1:6161" )
+ws.connect( "ws://127.0.0.1:10080" )
+ws.send( "ping" )
 
 #ws.send( json.dumps( { "type": "python-new-error" , "message": "this is a test error message" } ) )
-ws.send( json.dumps( { "type": "python-new-record" , "message": "MIN_MOTION_FRAMES >= 4" } ) )
+#ws.send( json.dumps( { "type": "python-new-record" , "message": "MIN_MOTION_FRAMES >= 4" } ) )
