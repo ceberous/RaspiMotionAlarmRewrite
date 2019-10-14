91ec9232ec4b8fdbad81cac88b132a75f28fa6bb
morpheous <morpheous@morpheouss-MacBook-Air.local>
2019-10-13 20:39:54 -0700

    21

--- redis-websocket-server/websocket_manager.js
+++ redis-websocket-server/websocket_manager.js
@@ -1,8 +1,9 @@
+const RedisUtils = require( "redis-manager-utils" );
+const redis_manager = new RedisUtils( 1 , "localhost" , 6379 );
+await redis_manager.init();
+
 function ON_CONNECTION( socket , req ) {
 	socket.on( "message" , async ( message )=> {
-		const RedisUtils = require( "redis-manager-utils" );
-		const redis_manager = new RedisUtils( 1 , "localhost" , 6379 );
-		await redis_manager.init();
 		try { message = JSON.parse( message ); }
 		catch( e ) { var a = message; message = { "type": a }; }
 		console.log( message );
@@ -13,7 +14,7 @@ function ON_CONNECTION( socket , req ) {
 			return new Promise( async function( resolve , reject ) {
 				try {
 					const count = message.count || 1;
-					redis_manager.redis.lrange( message.list_key , 0 , count , ( error , results )=> {
+					redis_manager.redis.lrange( message.list_key , 0 , ( count - 1 ) , ( error , results )=> {
 						console.log( results );
 						socket.send( "new_frames" , results );
 						resolve( results );
