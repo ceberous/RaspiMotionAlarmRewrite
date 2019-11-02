a9a9135a7f3ecd8104e9c5fe5a406c43f68390a5
morpheous <morpheous@morpheouss-MacBook-Air.local>
2019-11-02 12:14:37 -0700

    121

--- redis-websocket-server/websocket_manager.js
+++ redis-websocket-server/websocket_manager.js
@@ -108,8 +108,9 @@ function ON_CONNECTION( socket , req ) {
 					if ( !message.list_key ) { resolve(); return; }
 					if ( !message.channel ) { resolve(); return; }
 					const starting_position = message.starting_position || 0;
-					const ending_position = message.ending_position || 1;
+					const ending_position = message.ending_position || -1;
 					const result = await redis_get_lrange( message.list_key , starting_position , ending_position );
+					console.log( result );
 					socket.send( JSON.stringify( { message: `new_${ pluralize( message.channel ) }` , data: result } ) );
 					resolve( result );
 					return;
