057b2d974bf7e6884001abd4bcd60c35cc2f12ee
morpheous <morpheous@morpheouss-MacBook-Air.local>
2019-11-03 20:55:38 -0800

    141

--- py_scripts/motion_simple_rewrite_fixed.py
+++ py_scripts/motion_simple_rewrite_fixed.py
@@ -323,22 +323,21 @@ class TenvisVideo():
 			thresh = cv2.threshold( frameDelta , delta_thresh , 255 , cv2.THRESH_BINARY )[ 1 ]
 			thresh = cv2.dilate( thresh , None , iterations=2 )
 
-			frame_retval , frame_buffer = cv2.imencode( '.jpg' , frame )
-			frame_base64 = base64.b64encode( frame_buffer )
-			thresh_retval , thresh_buffer = cv2.imencode( '.jpg' , thresh )
-			thresh_base64 = base64.b64encode( thresh_buffer )
-			delta_retval , delta_buffer = cv2.imencode( '.jpg' , frameDelta )
-			delta_base64 = base64.b64encode( delta_buffer )
-			redis_manager.publish( "python-script-controller" , json.dumps({
-				"channel": "new_frame" , "data64": frame_base64
-			}))
-			redis_manager.publish( "python-script-controller" , json.dumps({
-				"channel": "new_threshold" , "data64": thresh_base64
-			}))
-			redis_manager.publish( "python-script-controller" , json.dumps({
-				"channel": "new_delta" , "data64": delta_base64
-			}))
-
+			# frame_retval , frame_buffer = cv2.imencode( '.jpg' , frame )
+			# frame_base64 = base64.b64encode( frame_buffer )
+			# thresh_retval , thresh_buffer = cv2.imencode( '.jpg' , thresh )
+			# thresh_base64 = base64.b64encode( thresh_buffer )
+			# delta_retval , delta_buffer = cv2.imencode( '.jpg' , frameDelta )
+			# delta_base64 = base64.b64encode( delta_buffer )
+			# redis_manager.publish( "python-script-controller" , json.dumps({
+			# 	"channel": "new_frame" , "data64": frame_base64
+			# }))
+			# redis_manager.publish( "python-script-controller" , json.dumps({
+			# 	"channel": "new_threshold" , "data64": thresh_base64
+			# }))
+			# redis_manager.publish( "python-script-controller" , json.dumps({
+			# 	"channel": "new_delta" , "data64": delta_base64
+			# }))
 
 			# Search for Movment
 			( cnts , _ ) = cv2.findContours( thresh.copy() , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE )
@@ -384,13 +383,13 @@ class TenvisVideo():
 				delta_retval , delta_buffer = cv2.imencode( '.jpg' , frameDelta )
 				delta_base64 = base64.b64encode( delta_buffer )
 				redis_manager.publish( "python-script-controller" , json.dumps({
-					"channel": "new_frame" , "data64": frame_base64
+					"channel": "images.frames" , "message": "new frame" , "image_b64": frame_base64
 				}))
 				redis_manager.publish( "python-script-controller" , json.dumps({
-					"channel": "new_threshold" , "data64": thresh_base64
+					"channel": "images.thresholds" , "message": "new threshold" , "image_b64": thresh_base64
 				}))
 				redis_manager.publish( "python-script-controller" , json.dumps({
-					"channel": "new_delta" , "data64": delta_base64
+					"channel": "images.deltas" , "message": "new delta" , "image_b64": delta_base64
 				}))
 				#redis_publish( { "channel": "command" , "command": "publish_new_image_set" , "message": "Saving New Image Set" } )
 
--- redis-websocket-server/python_script_subscriber.js
+++ redis-websocket-server/python_script_subscriber.js
@@ -1,4 +1,3 @@
-
 function INITIALIZE() {
 	return new Promise( async function( resolve , reject ) {
 		try {
@@ -6,7 +5,23 @@ function INITIALIZE() {
 			const RedisUtils = require( "redis-manager-utils" );
 			const Publishing = require( "../server/RedisPublishingManager.js" );
 
-			function python_publish( options ) {
+			const tweetnacl = require( "tweetnacl" );
+			tweetnacl.util = require( "tweetnacl-util" );
+			tweetnacl.sealedbox = require( "tweetnacl-sealedbox-js" );
+
+			console.log( "Starting" );
+			const redis_manager = new RedisUtils( Personal.redis.database_number , Personal.redis.host , Personal.redis.port  );
+			await redis_manager.init();
+
+			function encrypt( message ) {
+				const publicKeyBinary = tweetnacl.util.decodeBase64( Personal.libsodium.public_key );
+				const messageUTF8 = ( new util.TextEncoder( "utf-8" ) ).encode( message );
+				const encryptedBinary = tweetnacl.sealedbox.seal( messageUTF8 , publicKeyBinary );
+				const encrypted = tweetnacl.util.encodeBase64( encryptedBinary );
+				return encrypted;
+			}
+
+			function publish_log( options ) {
 				try {
 					const global_log_options = {
 						...options ,
@@ -25,9 +40,30 @@ function INITIALIZE() {
 				catch( error ) { console.log( error ); }
 			}
 
-			console.log( "Starting" );
-			const redis_manager = new RedisUtils( Personal.redis.database_number , Personal.redis.host , Personal.redis.port  );
-			await redis_manager.init();
+			function publish_image( options ) {
+				const now = new Date( new Date().toLocaleString( "en-US" , { timeZone: "America/New_York" } ) );
+				const dd = String( now.getDate()).padStart( 2 , '0' );
+				const mm = String( now.getMonth() + 1 ).padStart( 2 , '0' );
+				const yyyy = now.getFullYear();
+				const hours = String( now.getHours() ).padStart( 2 , '0' );
+				const minutes = String( now.getMinutes() ).padStart( 2 , '0' );
+				const seconds = String( now.getSeconds() ).padStart( 2 , '0' );
+				const list_key = `sleep.${ options.channel }.${ yyyy }.${ mm }.${ dd }`;
+				const time_stamp_string = `${ yyyy }.${ mm }.${ dd } @@ ${ hours }:${ minutes }:${ seconds }`;
+				const Custom_JSON_Object = JSON.stringify({
+					...options ,
+					...{
+						timestamp: now ,
+						list_key: list_key ,
+						time_stamp_string: time_stamp_string ,
+						message: `${ time_stamp_string } === ${ options.message }`
+					}
+				});
+				const encrypted = encrypt( Custom_JSON_Object );
+				console.log( encrypted );
+				await redis_manager.listLPUSH( list_key , encrypted );
+			}
+
 			redis_manager.redis.on( "message" , function ( channel , message ) {
 					//console.log( "sub channel " + channel + ": " + message );
 					console.log( "new message from: " + channel );
@@ -35,14 +71,19 @@ function INITIALIZE() {
 					try {
 						message = JSON.parse( message );
 						console.log( message );
-						python_publish( message );
-						if ( message.command ) {
-							switch ( message.command ) {
-								case "publish_new_image_set":
-									PublisherManager.new_image_set();
+						if ( message.channel ) {
+							switch ( message.channel ) {
+								case "log":
+									publish_log( message );
 									break;
-								case "publish_new_frame":
-									PublishingManager.new_frame();
+								case "images.frames":
+									publish_image( message );
+									break;
+								case "images.thresholds":
+									publish_image( message );
+									break;
+								case "images.deltas":
+									publish_image( message );
 									break;
 								default:
 									break;
