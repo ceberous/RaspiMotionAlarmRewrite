fc281271aab9c4a64e52946cfa8bffd490f1350f
morpheous <morpheous@MacBook-Air.local>
2019-11-07 17:06:42 -0800

    187

--- py_scripts/motion_simple_rewrite_fixed.py
+++ py_scripts/motion_simple_rewrite_fixed.py
@@ -288,7 +288,7 @@ class TenvisVideo():
 		self.total_motion = LOADED_CONFIG[ 'MOTION_EVENTS_ACCEPTABLE' ] + 1
 		now = datetime.now( eastern_tz )
 		self.EVENT_POOL = [
-			now + timedelta( minutes=10 )
+			now + timedelta( minutes=10 ) ,
 			now + timedelta( minutes=9 ) ,
 			now + timedelta( minutes=8 ) ,
 			now + timedelta( minutes=7 ) ,
@@ -298,7 +298,7 @@ class TenvisVideo():
 			now + timedelta( seconds=60 ) ,
 			now + timedelta( seconds=40 ) ,
 			now + timedelta( seconds=30 ) ,
-			now + timedelta( seconds=3 ) ,
+			now + timedelta( seconds=3 )
 		]
 
 	def motionTracking( self ):
