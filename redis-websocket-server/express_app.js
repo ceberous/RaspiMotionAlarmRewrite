81b89b263d7ded20854e707e86a9b30aff75149a
morpheous <morpheous@MacBook-Air.local>
2019-12-16 18:05:08 -0800

    221

--- redis-websocket-server/express_app.js
+++ redis-websocket-server/express_app.js
@@ -10,14 +10,17 @@ let app = express();
 app.use( basicAuth({
 	users: {
 		"e3c85a2b47ad45a106d903dbfa35c53489ee2ce17e2b44debab96a351bb82695": "4c2f70f42e9326b6c23da8ebff1097d4d80e0c9860c8ddbc022d55c2286d3b80"
-	}
+	} ,
+	challenge: true
 }));
-app.use( express.static( path.join( __dirname , "client" ) ) );
+//app.use( express.static( path.join( __dirname , "client" ) ) );
 //app.use( cors( { origin: "http://localhost:" + PORT.toString() } ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
-const HTMLPath = path.join( __dirname , "./client/views" , "index.html" );
+
+//const HTMLPath = path.join( __dirname , "./client/views" , "index.html" );
+const HTMLPath = path.join( "/home/morphs/TRANSFER/www/www" , "index.html" );
 
 app.get( "/" , function( req , res ) {
 	res.sendFile( HTMLPath );
